b58cba4d073718c4d97a067c1c6ed06edfbc6bef
sean_codes <sean.mann@alpinehomeair.com>
2017-01-09 22:58:33 -0600

    [UPDATE] - Not required loading sounds

--- engine/scr_core.js
+++ engine/scr_core.js
@@ -209,17 +209,8 @@ cs.sprite = {
 cs.sound = {
     list: {},
     load: function(soundPath, soundInfo = {}){
-        cs.sprite.loading += 1;
         var soundName = soundPath.split('/').pop();
         this.list[soundName] = new Audio(soundPath + '.ogg');
-
-        this.list[soundName].oncanplaythrough  = function(){
-            cs.sprite.loading -= 1;
-            if(cs.sprite.loading == 0){
-                cs.room.start();
-            }
-        }
-
     },
     play: function(sound, soundInfo = {}){
         this.list[sound].loop = soundInfo.loop || false;
