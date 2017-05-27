8fa10b1740f380adc15107500824481491c79623
sean_codes <sean.mann@alpinehomeair.com>
2017-05-27 15:50:42 -0500

    test

--- example/warrior_wip/objects/enviroment/obj_building.js
+++
@@ -1,9 +0,0 @@
-cs.objects['obj_building'] = {
-   width: 62,
-   height: 48,
-   sprite: 'spr_building',
-   create: function(){},
-   step: function(){
-      cs.draw.sprite(this.sprite, this.x, this.y)
-   }
-}
