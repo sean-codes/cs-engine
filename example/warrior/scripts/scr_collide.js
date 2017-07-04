3f11671105f67364b6731a1855ea05d34639ee8d
sean_codes <sean.mann@alpinehomeair.com>
2017-07-04 16:11:21 -0500

    Faster collision and remove x/y offset

--- example/warrior/scripts/scr_collide.js
+++ example/warrior/scripts/scr_collide.js
@@ -2,9 +2,9 @@ cs.script.collide = function(that, objtype, options){
    if(options == undefined) options = {}
    vspeed = (options.vspeed !== undefined) ? options.vspeed : that.vspeed || 0;
    hspeed = (options.hspeed !== undefined) ? options.hspeed : that.hspeed || 0;
-   var obj1top = (options.y || that.y) - that.yoff + vspeed;
+   var obj1top = (options.y || that.y) + vspeed;
    var obj1bottom = obj1top + (options.height || that.height);
-   var obj1left = (options.x || that.x) - that.xoff + hspeed;
+   var obj1left = (options.x || that.x) + hspeed;
    var obj1right = obj1left + (options.width || that.width);
    var i = cs.obj.list.length; while(i--){
       var obj2 = cs.obj.list[i]
@@ -15,7 +15,8 @@ cs.script.collide = function(that, objtype, options){
       var obj2bottum = obj2.y + obj2.height;
       var obj2left = obj2.x;
       var obj2right = obj2.x + obj2.width;
-      if ( obj1bottom <= obj2top || obj1top >= obj2bottum || obj1left >= obj2right || obj1right <= obj2left)
+
+      if(obj1bottom <= obj2top || obj1top >= obj2bottum || obj1left >= obj2right || obj1right <= obj2left)
          continue
 
       return obj2
