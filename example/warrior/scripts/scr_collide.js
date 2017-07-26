da196b35c1aa7cca48102bb9165958d37191647a
sean_codes <sean.mann@alpinehomeair.com>
2017-07-26 16:33:32 -0500

    testing a new collision function

--- example/warrior/scripts/scr_collide.js
+++ example/warrior/scripts/scr_collide.js
@@ -1,38 +1,35 @@
-cs.script.collide = function(that, objtype, options){
-   if(options == undefined) options = {}
-   vspeed = (options.vspeed !== undefined) ? options.vspeed : that.vspeed || 0;
-   hspeed = (options.hspeed !== undefined) ? options.hspeed : that.hspeed || 0;
-   var obj1top = (options.y || that.y) + vspeed;
-   var obj1bottom = obj1top + (options.height || that.height);
-   var obj1left = (options.x || that.x) + hspeed;
-   var obj1right = obj1left + (options.width || that.width);
-   var i = cs.obj.list.length; while(i--){
-      var obj2 = cs.obj.list[i]
-      if (obj2.live !== true || obj2.id == that.id || obj2.type !== objtype)
-         continue
+cs.script.collide = {
+   obj: function(obj1, objtype){
+      var i = cs.obj.list.length; while(i--){
+         var obj2 = cs.obj.list[i]
+         if (obj2.live !== true || obj2.id == obj1.id || obj2.type !== objtype)
+            continue
 
-      var obj2top = obj2.y;
-      var obj2bottum = obj2.y + obj2.height;
-      var obj2left = obj2.x;
-      var obj2right = obj2.x + obj2.width;
+         if(!this.rectInRect(
+            { x: obj1.x, y: obj1.y, width: obj1.width, height: obj1.height },
+            { x: obj2.x, y: obj2.y, width: obj2.width, height: obj2.height }))
+            continue
 
-      if(obj1bottom <= obj2top || obj1top >= obj2bottum || obj1left >= obj2right || obj1right <= obj2left)
-         continue
-
-      return obj2
-   }
-   return undefined
-}
-
-cs.script.collideRect = function(objtype, options){
-   var i = cs.obj.list.length; while(i--){
-      var obj = cs.obj.list[i];
-      //Damn this is kind of creepy looking :]
-      if(obj.live === true && (obj.type == objtype || objtype == '')){
-         if(options.x + options.width > obj.x && options.x < obj.x + obj.width
-            && options.y + options.height > obj.y && options.y < obj.y + obj.height)
-               return obj;
+         return obj2
       }
+      return undefined
+   },
+   rect: function(objtype, options){
+      var i = cs.obj.list.length; while(i--){
+         var obj = cs.obj.list[i];
+         //Damn this is kind of creepy looking :]
+         if(obj.live === true && (obj.type == objtype || objtype == '')){
+            if(options.x + options.width > obj.x && options.x < obj.x + obj.width
+               && options.y + options.height > obj.y && options.y < obj.y + obj.height)
+                  return obj;
+         }
+      }
+      return undefined;
+   },
+   rectInRect(rect1, rect2){
+      if(rect1.x >= rect2.x + rect2.width || rect1.x + rect1.width <= rect2.x ||
+         rect1.y >= rect2.y + rect2.height || rect1.y + rect1.height <= rect2.y )
+         return false
+      return true
    }
-   return undefined;
 }
