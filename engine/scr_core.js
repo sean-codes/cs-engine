835f9a001e596ffb8d512a9d484d8576ab3c302f
sean_codes <sean.mann@alpinehomeair.com>
2017-07-27 14:21:48 -0500

    change math sign

--- engine/scr_core.js
+++ engine/scr_core.js
@@ -1257,13 +1257,13 @@ cs.storage = {
 //---------------------------------------------------------------------------------------------//
 cs.math = {
     sign : function(number){
-        return (number >= 0) ? 1 : -1;
+        return number < 0 ? -1 : 1
     },
     iRandomRange : function(min, max) {
         return Math.round(min + Math.random()*(max-min))
     },
     choose: function(array){
-        return array[this.iRandomRange(0, array.length-1)];
+        return array[this.iRandomRange(0, array.length-1)]
     }
 }
 //---------------------------------------------------------------------------------------------//
