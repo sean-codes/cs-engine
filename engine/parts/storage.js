886556b860917a72eaabd8b5cab2d30dae8b37de
sean-codes <sean_codes@outlook.com>
2017-09-04 17:42:14 -0500

    read/write/group functions

--- engine/parts/storage.js
+++ engine/parts/storage.js
@@ -2,27 +2,28 @@
 //----------------------------------| Storage Functions |--------------------------------------//
 //---------------------------------------------------------------------------------------------//
 cs.storage = {
-   load: function(info){
-      var that = this
-      var name = info.path.split('/').pop()
-      var ajax = new XMLHttpRequest()
-      cs.load.add()
-      ajax.onreadystatechange = function() {
-         if(this.readyState == 4){
-            var data = JSON.parse(this.responseText)
-            if(info.group && !that[info.group]) that[info.group] = {}
-
-            info.group
-               ? that[info.group][info.name] = data
-               : that[info.name] = data
-
-            cs.load.check()
-         }
+   data: {},
+   init: function(){
+      for(var storage of cs.storages){
+         this.data[storage.name] = storage.data
       }
-      ajax.open("POST", `./${info.path}.json`, true)
-      ajax.send()
    },
    cache: function(){
       //we could cache something to local storage here
+   },
+   group: function(groupName){
+      var group = []
+      for(var storageName of Object.keys(this.data)){
+         if(storageName.startsWith(groupName)){
+            group.push(storageName)
+         }
+      }
+      return group
+   },
+   read: function(name){
+      return this.data[name]
+   },
+   write: function(info){
+      this.data[info.location] = info.data
    }
 }
