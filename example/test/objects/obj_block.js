fd0972a4d4ab733bcaedf41e07394b98f7fd164f
sean_codes <sean.mann@alpinehomeair.com>
2017-09-07 12:07:23 -0500

    getting text setup and testing

--- example/test/objects/obj_block.js
+++ example/test/objects/obj_block.js
@@ -2,9 +2,8 @@ cs.objects['obj_block'] = {
 	create: function(){
 		this.width = 16;
     	this.height = 16;
-		this.draw = 'gui'
 	},
 	step: function(){
-		cs.draw.sprite({ spr:'spr_block', x:this.x, y:this.y, width:40, aspectLock: true });
+		cs.draw.sprite({ spr:'spr_block', x: 20, y:20 });
 	}
 }
