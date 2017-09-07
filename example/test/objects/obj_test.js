e52735382764aea3d6ba874d5012372bd5a186db
sean-codes <sean_codes@outlook.com>
2017-09-07 12:52:21 -0500

    rename test

--- example/test/objects/obj_test.js
+++ example/test/objects/obj_test.js
@@ -1,7 +1,12 @@
-cs.objects['obj_block'] = {
+cs.objects['obj_test'] = {
 	create: function(){
 		this.width = 16;
     	this.height = 16;
+		this.text = cs.text.create({
+			text: 'some text',
+			lineHeight: 10,
+			width: 50
+		})
 	},
 	step: function(){
 		cs.draw.sprite({ spr:'spr_block', x: 20, y:20 });
