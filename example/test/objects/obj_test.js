f645b3bf5615e610e8f238f939edbd40c5d31d5d
sean-codes <sean_codes@outlook.com>
2017-09-08 16:35:49 -0500

    drawing lines

--- example/test/objects/obj_test.js
+++ example/test/objects/obj_test.js
@@ -5,12 +5,11 @@ cs.objects['obj_test'] = {
 
 		this.myText = {
 			text: 'some text cats meow fish',
-			lineHeight: 10,
 			width: 50
 		}
 	},
 	step: function(){
 		var textInfo = cs.draw.textInfo(this.myText)
-		cs.draw.text({ lines: textInfo.lines, lineHeight: textInfo.lineHeight, x: 20, y:20 });
+		cs.draw.text({ lines: textInfo.lines, x: 20, y:20 });
 	}
 }
