f28bf8b0a234caf13f3679dc5176c4a850dca79d
sean-codes <sean_codes@outlook.com>
2017-09-08 11:23:23 -0500

    test the textInfo function

--- example/test/objects/obj_test.js
+++ example/test/objects/obj_test.js
@@ -2,14 +2,15 @@ cs.objects['obj_test'] = {
 	create: function(){
 		this.width = 16;
     	this.height = 16;
-		this.text = cs.text.create({
-			name: 'test',
+
+		this.myText = {
 			text: 'some text cats meow fish',
 			lineHeight: 10,
 			width: 50
-		})
+		}
 	},
 	step: function(){
-		cs.draw.text({ test: true, text:'test', x: 20, y:20 });
+		var textInfo = cs.draw.textInfo(this.myText)
+		cs.draw.text({ lines: textInfo.lines, lineHeight: textInfo.lineHeight, x: 20, y:20 });
 	}
 }
