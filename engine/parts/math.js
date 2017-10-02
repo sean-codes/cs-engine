9c9ad7efddf9b8a5c19814bdd19e4d66e75e7c46
sean-codes <sean_codes@outlook.com>
2017-10-01 23:46:42 -0500

    add comment to remember what that is for

--- engine/parts/math.js
+++ engine/parts/math.js
@@ -12,6 +12,7 @@ cs.math = {
         return array[this.iRandomRange(0, array.length-1)]
     },
     chooseRatio: function(ratios){
+      // ratios = { "Choice": 50, "Choice 2": 100 }
       var test = Math.random() * 100
       for(var ratio in ratios){
          if(test < ratios[ratio]){
