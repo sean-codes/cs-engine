06b92b1e8fda6304a7d1180a890b4afd47c01d9d
sean-codes <sean_codes@outlook.com>
2017-10-03 20:32:26 -0500

    beautify and add key functions

--- engine/parts/keys.js
+++ engine/parts/keys.js
@@ -2,63 +2,72 @@
 //---------------------------------| Key Input Functions |-------------------------------------//
 //---------------------------------------------------------------------------------------------//
 cs.key = {
-    up : {},
-    down : {},
-    held : {},
-    events : [],
-    addEvent : function(keyCode, eventType){
-        var num = cs.key.events.length
-        cs.key.events[num] = {
-            event : eventType,
-            key : keyCode
-        }
-    },
-    execute : function(){
-        for(var i = 0; i < cs.key.events.length; i++){
-            var event = cs.key.events[i].event;
-            var key = cs.key.events[i].key
-            cs.key.processEvent(key, event)
-        }
-        cs.key.events = [];
-    },
-    processEvent : function(keyCode, type){
-       if(type == 'up'){
-          if(!cs.key.up[keyCode])
-            cs.key.up[keyCode] = true
-          return
-       }
+   upList: {},
+   downList: {},
+   heldList: {},
+   events: [],
+   addEvent: function(keyCode, eventType) {
+      var num = cs.key.events.length
+      cs.key.events[num] = {
+         event: eventType,
+         key: keyCode
+      }
+   },
+   execute: function() {
+      for (var i = 0; i < cs.key.events.length; i++) {
+         var event = cs.key.events[i].event;
+         var key = cs.key.events[i].key
+         cs.key.processEvent(key, event)
+      }
+      cs.key.events = [];
+   },
+   processEvent: function(keyCode, type) {
+      if (type == 'up') {
+         console.log('wtf')
+         cs.key.upList[keyCode] = true
+         return
+      }
 
-      cs.key.down[keyCode] = true;
-      cs.key.held[keyCode] = true;
-    },
-    updateDown : function(keyEvent){
-        keyEvent.preventDefault();
-        if(!keyEvent.repeat){
-            var key = keyEvent.keyCode;
-            cs.key.virtualDown(key);
-        }
-    },
-    updateUp : function(keyEvent){
-        var key = keyEvent.keyCode;
-        cs.key.virtualUp(key);
-    },
-    virtualDown : function(keyCode){
-        cs.key.addEvent(keyCode, 'down');
-    },
-    virtualUp : function(keyCode){
-        cs.key.addEvent(keyCode, 'up');
-    },
-    virtualPress : function(key){
-        this.virtualDown(key);
-        this.virtualUp(key);
-    },
-    reset : function(){
-        for(var tmp in cs.key.down){
-            cs.key.down[tmp] = false
-            if(cs.key.up[tmp])
-                cs.key.held[tmp] = false
+      cs.key.downList[keyCode] = true;
+      cs.key.heldList[keyCode] = true;
+   },
+   reset: function() {
+      for (var tmp in cs.key.downList) {
+         cs.key.downList[tmp] = false
+         if (cs.key.upList[tmp])
+            cs.key.heldList[tmp] = false
 
-            cs.key.up[tmp] = false
-        }
-    }
+         cs.key.upList[tmp] = false
+      }
+   },
+   updateDown: function(keyEvent) {
+      keyEvent.preventDefault();
+      if (!keyEvent.repeat) {
+         var key = keyEvent.keyCode;
+         cs.key.virtualDown(key);
+      }
+   },
+   updateUp: function(keyEvent) {
+      var key = keyEvent.keyCode;
+      cs.key.virtualUp(key);
+   },
+   virtualDown: function(keyCode) {
+      cs.key.addEvent(keyCode, 'down');
+   },
+   virtualUp: function(keyCode) {
+      cs.key.addEvent(keyCode, 'up');
+   },
+   virtualPress: function(key) {
+      this.virtualDown(key);
+      this.virtualUp(key);
+   },
+   up: function(keyID) {
+      return cs.key.upList[keyID] || false
+   },
+   down: function(keyID) {
+      return cs.key.downList[keyID] || false
+   },
+   held: function(keyID) {
+      return cs.key.heldList[keyID] || false
+   }
 }
