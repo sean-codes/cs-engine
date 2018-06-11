85baf0b0e4f14c4a45d3aa786c2f77dd01fbca59
Sean Codes <sean_codes@outlook.com>
2018-06-11 00:56:24 -0500

    allow using top, left, right, bottom for mask

--- engine/parts/sprite.js
+++ engine/parts/sprite.js
@@ -25,10 +25,10 @@ cs.sprite = {
          xoff: options.xoff || 0,
          yoff: options.yoff || 0,
          mask: {
-            x: options.mask ? (options.mask.x || 0) : 0,
-            y: options.mask ? (options.mask.y || 0) : 0,
-            width: options.mask ? (options.mask.width || width) : width,
-            height: options.mask ? (options.mask.height || height) : height
+            x: options.mask ? (options.mask.x || options.mask.left || 0) : 0,
+            y: options.mask ? (options.mask.y || options.mask.top || 0) : 0,
+            width: options.mask ? (options.mask.width || width - (options.mask.left || 0) - (options.mask.right || 0)) : width,
+            height: options.mask ? (options.mask.height || height - (options.mask.top || 0) - (options.mask.bottom || 0)) : height
          },
          frames : []
       }
@@ -109,7 +109,12 @@ cs.sprite = {
 			yoff: sprite.yoff,
          frames: options.texture ? [sprite.texture] : sprite.frames,
          frame: options.frame,
-         mask: { x:sprite.mask.x, y:sprite.mask.y, width:sprite.mask.width, height:sprite.mask.height }
+         mask: {
+				x:sprite.mask.x,
+				y:sprite.mask.y,
+				width:sprite.mask.width,
+				height:sprite.mask.height
+			}
       }
    }
 }
