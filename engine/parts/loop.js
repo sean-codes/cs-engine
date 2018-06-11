ee9c454193a25f6c1bcee6793f1ac2f7aa4731c0
Sean Codes <sean_codes@outlook.com>
2018-06-11 00:56:45 -0500

    add draw after step

--- engine/parts/loop.js
+++ engine/parts/loop.js
@@ -13,14 +13,25 @@ cs.loop = {
       cs.surface.clearAll()
 		cs.obj.addNewObjects()
 
-      var i = cs.obj.list.length; while(i--){
+		var i = cs.obj.list.length; while(i--){
+         if(cs.obj.list[i].core.live){
+            var obj = cs.obj.list[i];
+				cs.draw.setSurface(obj.core.surface);
+            cs.particle.settings = obj.core.particle.settings;
+            cs.particle.obj = obj;
+            var step = cs.objects[obj.core.type].step;
+            step && step.call(obj);
+         }
+      }
+
+		var i = cs.obj.list.length; while(i--){
          if(cs.obj.list[i].core.live){
             var obj = cs.obj.list[i];
             cs.draw.setSurface(obj.core.surface);
             cs.particle.settings = obj.core.particle.settings;
             cs.particle.obj = obj;
-            var step = cs.objects[obj.core.type].step;
-            step.call(obj);
+            var draw = cs.objects[obj.core.type].draw;
+            draw && draw.call(obj);
          }
       }
 
