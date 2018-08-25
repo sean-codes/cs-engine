bc60ed2b321d94e4efd0b16fd458de363eb21b5a
Sean Codes <sean_codes@outlook.com>
2018-08-24 21:05:34 -0500

    old

--- engine/parts/math.js
+++ engine/parts/math.js
@@ -3,6 +3,7 @@
 //---------------------------------------------------------------------------------------------//
 cs.math = {
     sign : function(number){
+	     if(!number) return 0
         return number < 0 ? -1 : 1
     },
     iRandomRange : function(min, max) {
@@ -12,7 +13,7 @@ cs.math = {
         return array[this.iRandomRange(0, array.length-1)]
     },
     chooseRatio: function(ratios){
-      // ratios = {"50": "Choice1", "100": "Choice2"}
+      // ratios = { "50": "Choice1", "100": "Choice2" }
       var random = Math.random() * 100
       for(var ratio in ratios){
          if(parseInt(ratio) > random){
